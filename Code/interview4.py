c26586bfce5682f6434b15b153e249fda3c56406
Dinh Long Le <111885994+long-noob@users.noreply.github.com>
2023-10-01 17:41:38 +0700

    Update interview4.py

--- Code/interview4.py
+++ Code/interview4.py
@@ -1,12 +1,9 @@
 def are_anagrams(word1, word2):
-    # Remove spaces and convert to lowercase for case insensitivity
     word1 = word1.replace(" ", "").lower()
     word2 = word2.replace(" ", "").lower()
 
-    # Check if the sorted characters of the words are the same
-    return sorted(word1) == sorted(word2)
+   return sorted(word1) == sorted(word2)
 
-# Example usage:
 word1 = "ngu"
 word2 = "ung"
 result = are_anagrams(word1, word2)
